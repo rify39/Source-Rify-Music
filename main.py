1b827384f78cbfd88365424a7a50f6b3222be1dd
ࢪيفــٰآي <98587160+rify39@users.noreply.github.com>
2022-02-08 10:29:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from pytgcalls import idle
+
+from config import call_py
+
+from MusicRify.التشغيل import arq
+
+async def main():
+
+    await call_py.start()
+
+    print("""    ------------------
+
+   | ميوزك ريفآي الان شغال ! |
+
+    ------------------"""    )
+
+    await idle()
+
+    await arq.close()
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(main())
